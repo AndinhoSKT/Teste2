c2f9846f35c757b0cb6026d014e015100c6bd78d
Anderson <Anderson@DESKTOP-ELPT3D7>
2024-03-14 20:01:48 -0300

    Adicionar arquivos de projeto.

---
+++ Teste2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program {
+    static void Main() {
+        string entrada = "Hello, world!";
+        string invertida = "";
+
+        for (int i = entrada.Length - 1; i >= 0; i--) {
+            invertida += entrada[i];
+        }
+
+        Console.WriteLine("String original: " + entrada);
+        Console.WriteLine("String invertida: " + invertida);
+    }
+}
+
